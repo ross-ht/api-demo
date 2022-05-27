40e14354c62b526891ba95c5bea778edb7c28d41
ross.H <rosstaohu@gmail.com>
2022-05-27 12:16:53 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # api-demo
+## go版本 api的调用
+说明：
+**公共接口**
+进入main.go中，无参数的请求直接调用
+带参数的请求需要在params中输入json格式的参数 如：`{"symbol":"BTCUSDT",	"limit":"200"}`
+**私有接口**
+需要先在config.go中配置相关的api_key和sec_key
+后续操作与公共接口相同
+
  
--- main.go
+++ main.go
@@ -6,21 +6,20 @@ import (
 	spotList "github.com/ross-ht/api-demo/spot"
 )
 
+//说明：
+//在params中输入json格式的参数 如：`{"symbol":"BTCUSDT",	"limit":"200"}`
+var params string = `{"symbol":"BTCUSDT",	"limit":"200"}`
+
 func main() {
-	//说明：
-	//在``中输入json格式的参数 如：{"symbol":"BTCUSDT",	"limit":"200"}
-	//----------------------------------------------//
-	// depthinfo := spotList.SpotMarketDepth(`{
-	// 	"symbol":"BTCUSDT",
-	// 	"limit":"200"
-	// }`)
-	// fmt.Println("返回信息:", depthinfo)
+
+	depthinfo := spotList.SpotMarketDepth(params)
+	fmt.Println("返回信息:", depthinfo)
 
 	// accountInfo := spotList.SpotmyTrade(`{
 	// 	"symbol":"MXUSDT"
 	// }`)
 	// fmt.Println("返回信息:", accountInfo)
-	subAccount := spotList.CreateSub(`{ "subAccount":"subAccount333","note":"no3"}`)
-	fmt.Println("返回信息:", subAccount)
+	// subAccount := spotList.CreateSub(params)
+	// fmt.Println("返回信息:", subAccount)
 
 }
